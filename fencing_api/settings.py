eddda1253c66006f0957d58b2f296bb7af32cbde
chungisaac1 <isaac815@naver.com>
2024-04-10 17:31:30 +0900

    11th

--- fencing_api/settings.py
+++ fencing_api/settings.py
@@ -223,6 +223,6 @@ SIMPLE_JWT = {
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
-# CORS_ORIGIN_WHITELIST = (
-#     'http://localhost:8081',
-# )
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:8081',
+)
