06d20c7793f868e0d2100e44ea5f4eda36209468
chung isaac <isaac815@naver.com>
2024-08-03 21:11:17 +0900

    version

--- fencing_api/settings.py
+++ fencing_api/settings.py
@@ -28,7 +28,6 @@ def get_secret(setting):
 
 SECRET_KEY = get_secret("SECRET_KEY")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 # settings.py
 
 
